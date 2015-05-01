b1caefdab23c165b5430ad0704b1feb174fcc640
Kamil Baldyga <kbaldyga@travix.com>
2015-05-01 15:23:05 +0200

    Added Suave implementation of /hello

---
+++ src/Restival.Api.Suave/HelloService.fs
@@ -0,0 +1,25 @@
+﻿module HelloService
+
+open Suave
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Successful
+open Suave.Json
+open Suave.Types
+open Suave.Web
+open System.Net
+
+open Restival.Api.Common.Resources
+
+let config = { defaultConfig with 
+                bindings = [HttpBinding.mk HTTP IPAddress.Loopback <| Sockets.Port.Parse "8080"] }
+let serialize<'T> = System.Text.Encoding.ASCII.GetString << toJson
+
+let app =
+  choose
+    [ GET >>= Writers.setMimeType "application/json" >>= choose 
+        [ path "/hello" >>= OK (serialize <| Greeting() )
+          pathScan "/hello/%s" (fun name -> OK (serialize <| Greeting(name)))]
+    ]
+
+let start _ = startWebServer config app
---
+++ src/Restival.ApiTests/Suave/SuaveApiTests.cs
@@ -0,0 +1,7 @@
+﻿namespace Restival.ApiTests.Suave {
+    public class SuaveApiTests : ApiTestsBase {
+        protected override string BaseUri {
+            get { return ("http://restival.local:8080"); }
+        }
+    }
+}
