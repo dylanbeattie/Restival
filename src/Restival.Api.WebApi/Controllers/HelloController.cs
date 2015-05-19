18f71fd6e1a291ac50a1a5048a35aea7824a6aa2
Dylan Beattie <dylan@dylanbeattie.net>
2015-05-19 22:50:28 +0100

    Enabled attribute routing in WebAPI implementation
    This meant upgrading Restival.WebAPI to .NET 4.5, which in turn meant upgrading Microsoft.Net.Http and a whole load of other packages. Fun :)

--- src/Restival.Api.WebApi/Controllers/HelloController.cs
+++ src/Restival.Api.WebApi/Controllers/HelloController.cs
@@ -3,6 +3,7 @@ using Restival.Api.Common.Resources;
 
 namespace Restival.Api.WebApi.Controllers {
     public class HelloController : ApiController {
+        [Route("hello/{name=World}")]
         public Greeting Get(string name) {
             return (new Greeting(name));
         }
