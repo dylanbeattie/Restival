b7553cf6fc9f183eff43f57e842d549c0c55a3cf
Dylan Beattie <dylan@dylanbeattie.net>
2015-06-30 00:20:28 +0100

    Moved NancyFX profile handler into its own file.

--- src/Restival.Api.Nancy/HelloModule.cs
+++ src/Restival.Api.Nancy/HelloModule.cs
@@ -1,28 +1,11 @@
-﻿using System.Linq;
-using Nancy;
-using Restival.Api.Common;
+﻿using Nancy;
 using Restival.Api.Common.Resources;
-using Restival.Data;
 
 namespace Restival.Api.Nancy {
     public class HelloModule : NancyModule {
         public HelloModule() {
             Get["/hello"] = _ => new HelloResponse { Message = new Greeting("World").Message };
             Get["/hello/{name}"] = parameters => new HelloResponse { Message = new Greeting(parameters.name).Message };
-        }
-    }
-
-    public class ProfileListModule : NancyModule {
-        private readonly IProfileDatabase db;
-
-
-        public ProfileListModule(IProfileDatabase db) {
-            this.db = db;
-            Get["/profiles"] = _ => {
-                var profiles = db.ListProfiles();
-                var result = new ProfileListResponse() { Items = profiles.Select(p => p.ToProfileResponse()).ToList() };
-                return (result);
-            };
         }
     }
 }
---
+++ src/Restival.Api.Nancy/ProfileListModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Nancy;
+using Restival.Api.Common;
+using Restival.Api.Common.Resources;
+using Restival.Data;
+
+namespace Restival.Api.Nancy {
+    public class ProfileListModule : NancyModule {
+        private readonly IProfileDatabase db;
+
+
+        public ProfileListModule(IProfileDatabase db) {
+            this.db = db;
+            Get["/profiles"] = _ => {
+                var profiles = db.ListProfiles();
+                var result = new ProfileListResponse() { Items = profiles.Select(p => ProfileExtensions.ToProfileResponse(p)).ToList() };
+                return (result);
+            };
+        }
+    }
+}
