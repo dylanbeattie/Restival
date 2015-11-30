b9e9bd32d677892c6124ed5cfda31e36ca4d32bd
Dylan Beattie <dylan.beattie@spotlight.com>
2015-11-30 15:44:10 +0000

    Replaced dynamic implementation of _links with Dictionary<string, Dictionary<string,string>>

---
+++ src/Restival.Api.Common/Resources/Resource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Restival.Api.Common.Resources {
+    public abstract class Resource {
+
+        protected Dictionary<string, Dictionary<string, string>> links;
+
+        [DataMember(Name = "_links")]
+        public Dictionary<string, Dictionary<string, string>> Links {
+            get {
+                if (links != null) return (links);
+                links = new Dictionary<string, Dictionary<string, string>>();
+                links.Add("self", new Dictionary<string, string> { { "href", SelfUrl } });
+                AddLinks(links);
+                return (links);
+            }
+            set { links = value; }
+        }
+
+        protected abstract void AddLinks(Dictionary<string, Dictionary<string, string>> linkset);
+        protected abstract string SelfUrl { get; }
+    }
+}
--- src/Restival.Api.Common/Resources/WhoAmIResponse.cs
+++ src/Restival.Api.Common/Resources/WhoAmIResponse.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.Runtime.Serialization;
 
 namespace Restival.Api.Common.Resources {
     [DataContract]
-    public class WhoAmIResponse {
+    public class WhoAmIResponse : Resource {
         [DataMember(Name = "id")]
         public int Id { get; set; }
 
@@ -13,19 +15,6 @@ namespace Restival.Api.Common.Resources {
         [DataMember(Name = "name")]
         public string Name { get; set; }
 
-        private dynamic links;
-
-        [DataMember(Name = "_links")]
-        public dynamic Links {
-            get {
-                return (links ?? new {
-                    self = new { href = String.Format("/users/{0}", Id) },
-                    agencies = new { href = String.Format("/users/{0}/agencies", Id) }
-                });
-            }
-            set { links = value; }
-        }
-
         public WhoAmIResponse(int id, string username, string name) {
             this.Id = id;
             this.Username = username;
@@ -33,5 +22,14 @@ namespace Restival.Api.Common.Resources {
         }
 
         public WhoAmIResponse() { }
+        protected override void AddLinks(Dictionary<string, Dictionary<string, string>> linkset) {
+            linkset.Add("agencies", new Dictionary<string, string> { { "href", String.Format("/users/{0}/agencies", Id) } });
+        }
+
+        protected override string SelfUrl {
+            get { return (String.Format("/users/{0}", this.Id)); }
+        }
     }
+
+
 }
