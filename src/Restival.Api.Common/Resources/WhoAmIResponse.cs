7390671be41ba70b809e121809548bd4a570fa91
dylanbeattie <dylan@dylanbeattie.net>
2015-12-07 11:11:50 +0000

    Testing different serialization hints and trying to work out why ServiceStack ignores DataMember attributes for dynamic types...

--- src/Restival.Api.Common/Resources/WhoAmIResponse.cs
+++ src/Restival.Api.Common/Resources/WhoAmIResponse.cs
@@ -15,12 +15,30 @@ namespace Restival.Api.Common.Resources {
         [DataMember(Name = "name")]
         public string Name { get; set; }
 
-        public WhoAmIResponse() { }
+        public WhoAmIResponse() {}
 
         public WhoAmIResponse(Guid id, string username, string name) {
             this.Id = id;
             this.Username = username;
             this.Name = name;
+        }
+
+        [DataMember(Name = "links")]
+        public dynamic _links
+        {
+            get { return new { self = "me!me!me!" }; }
+        }
+
+        [DataMember(Name = "ObjectFoo")]
+        public object Foo
+        {
+            get { return (new object()); }
+        }
+
+        [DataMember(Name = "_underscored")]
+        public object Underscored
+        {
+            get { return ("stringy!"); }
         }
     }
 }
--- src/Restival.ApiTests/WhoAmIApiTestsBase.cs
+++ src/Restival.ApiTests/WhoAmIApiTestsBase.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using Restival.Api.Common.Resources;
 using Restival.Data;
@@ -12,18 +13,22 @@ using Shouldly;
 
 namespace Restival.ApiTests {
     public abstract class WhoAmIApiTestsBase<TApi> : ApiTestBase<TApi> where TApi : IApiUnderTest, new() {
-        private WhoAmIResponse GetWhoAmI(string username = null, string password = null) {
+        private IRestResponse<WhoAmIResponse> GetWhoAmIResponse(string username, string password) {
             var client = new RestClient(BaseUri);
             var request = new RestRequest("whoami");
             request.AddHeader("Accept", "application/json");
             request.Credentials = new NetworkCredential(username, password);
             Console.WriteLine(client.BuildUri(request));
-            var status = client.Execute<WhoAmIResponse>(request);
-            foreach (var header in status.Headers) {
-                Console.WriteLine(header.Name + ": " + header.Value);
-            }
-            Console.WriteLine(status.Content);
-            return (status.Data);
+            return (client.Execute<WhoAmIResponse>(request));
+        }
+        private WhoAmIResponse GetWhoAmI(string username = null, string password = null) {
+            var response = GetWhoAmIResponse(username, password);
+            return (response.Data);
+        }
+
+        private string GetWhoAmIJson(string username = null, string password = null) {
+            var response = GetWhoAmIResponse(username, password);
+            return (response.Content);
         }
 
         [Test]
@@ -47,9 +52,8 @@ namespace Restival.ApiTests {
         }
 
         public IEnumerable<string[]> TestUsers {
-            get
-            {
-                return(FakeDataStore.Users.Select(u => new[] {
+            get {
+                return (FakeDataStore.Users.Select(u => new[] {
                     u.Id.ToString(),
                     u.Username,
                     u.Password,
@@ -77,6 +81,26 @@ namespace Restival.ApiTests {
         public void GET_WhoAmI_Includes_Name(string guid, string username, string password, string name) {
             var me = GetWhoAmI(username, password);
             me.Name.ShouldBe(name);
+        }
+
+        //[Test]
+        //[TestCaseSource("TestUsers")]
+        //public void GET_WhoAmI_Includes_Self_Link(string guid, string username, string password, string name) {
+        //    var me = GetWhoAmI(username, password);
+        //    var self = me.Links["self"]["href"];
+        //    var agencies = me.Links["agencies"]["href"];
+        //    self.ShouldBe("/users/" + guid, Case.Insensitive);
+        //    agencies.ShouldBe("/users/" + guid + "/agencies", Case.Insensitive);
+        //}
+
+        [Test]
+        [TestCaseSource("TestUsers")]
+        public void GET_WhoAmI_Includes_Exact_HAL_Json(string guid, string username, string password, string name) {
+            var json = GetWhoAmIJson(username, password);
+            var jo = JObject.Parse(json);
+            var links = jo.SelectToken("$._links");
+            links.ShouldNotBe(null);
+            Console.WriteLine(json);
         }
     }
 }
